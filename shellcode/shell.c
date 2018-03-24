ffeacef3da5382f58dcb9cc51442d9f7d92218a2
root <madisononealburke@gmail.com>
2018-03-24 20:09:30 +0000

    untangle cmd double pointer

--- shellcode/shell.c
+++ shellcode/shell.c
@@ -8,17 +8,16 @@
 int main(int argc, char **argv, char **env)
 {
 	size_t init_size = 1024;
-	char **cmdline;
+	char *cmdline;
 	char prompt[] = "$ ";
 
-	cmdline = malloc(sizeof(char *) * 1);
-	cmdline[0] = malloc(sizeof(char) * init_size);
+	cmdline = malloc(sizeof(char *) * init_size);
 
 	while (1)
 	{
 		printf("%s", prompt);
 
-		if (getline(cmdline, &init_size, stdin) == -1)
+		if (getline(&cmdline, &init_size, stdin) == -1)
 			if (ferror(stdin))
 				error("getline error");
 			else
@@ -27,9 +26,9 @@ int main(int argc, char **argv, char **env)
 				exit(0);
 			}
 		// remove trailing newline
-		cmdline[0][strlen(cmdline[0]) - 1] = '\0';
+		cmdline[strlen(cmdline) - 1] = '\0';
 
 		// evaluate command line
-		eval(cmdline[0], env);
+		eval(cmdline, env);
 	}
 }
